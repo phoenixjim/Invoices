131b7e32d9a7b5c3287376ca9f50534604503b72
phoenixjim <james.lefavour@zohomail.com>
2023-06-29 23:09:10 -0400

    Corrected position of report file, corrected conflict in DBUI

--- UI/DBUI.h
+++ UI/DBUI.h
@@ -153,11 +153,9 @@ class Invoices : public WithInvoicesLayout<TopWindow> {
 	
 	MenuBar		mainmenu;
 	// added 'save and mark paid' on create invoice window
-<<<<<<< HEAD
-	String version = "2.1.0.3";
-=======
-	String version = "2.1.1.0"; // added new report, renamed old profit and loss to yeartoyear
->>>>>>> 624dcb0c70b428e63e1669d7258e5acd9486717d
+
+	String version = "2.1.1.1"; // added new report, renamed old profit and loss to yeartoyear
+
 public:
 	Invoices();
 	void Paint(Draw& w) {
